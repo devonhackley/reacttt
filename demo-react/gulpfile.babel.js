9b331ebd987fa682b4adfa688662306260d0739c
devonhackley <devonhackley@gmail.com>
2017-09-08 12:46:36 -0700

    commiting previously worked files

--- demo-react/gulpfile.babel.js
+++ demo-react/gulpfile.babel.js
@@ -9,6 +9,8 @@ const changed = require('gulp-changed');
 
 const SCSS_SRC = './src/assets/scss/**/*.scss';
 const SCSS_DEST = './src/assets/css';
+const SCSS_FONT = './src/assets/font-awesome-4.7.0';
+const SCSS_FONT_DEST = './src/assets/fonts';
 
 gulp.task('compile_scss', () => {
   gulp.src(SCSS_SRC) //creating a string of readable files
@@ -19,9 +21,20 @@ gulp.task('compile_scss', () => {
   .pipe(gulp.dest(SCSS_DEST))
 });
 
+gulp.task('compile_fonts', () => {
+  return gulp.src(SCSS_FONT)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCSS())
+  .pipe(rename({ suffix: '.min'}))
+  .pipe(changed(SCSS_FONT_DEST))
+  .pipe(gulp.dest(SCSS_FONT_DEST))
+})
+
 //Detect changes in the scss files
 gulp.task('watch_scss', () => {
   gulp.watch(SCSS_SRC, ['compile_scss'])
+  gulp.watch(SCSS_FONT, ['compile_fonts'])
+
 })
 
 //runs all tasks
--- demo-react/src/components/pages/home/feature.js
+++ demo-react/src/components/pages/home/feature.js
@@ -5,22 +5,33 @@ class Feature extends Component {
     return (
       <section className="feature">
         <h3 className="text-center"> GET FOOD FAST NOT FAST FOOD</h3>
-        <p className="copy"> Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>
+        <p className="copy"> Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. </p>
 
         <div className="container">
           <div className="row">
             <div className="col-md-3">
               <p className="content-arrange">
-                Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+              <h4> Have Healthy Meals</h4>
+              <p className="fa fa-camera"></p>
+                Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earth.
               </p>
             </div>
             <div className="col-md-3">
               <p className="content-arrange">
-              Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane
+              <i className="fa fa-camera"></i>
+              <h4> Have Healthy Meals</h4>
+              Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water chestnut
               </p>
             </div>
             <div className="col-md-3">
               <p className="content-arrange">
+              <h4> Have Healthy Meals</h4>
+                Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish
+              </p>
+            </div>
+            <div className="col-md-3">
+              <p className="content-arrange">
+              <h4> Have Healthy Meals</h4>
                 Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot horseradish
               </p>
             </div>
