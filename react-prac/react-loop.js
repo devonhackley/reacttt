3e9c84ef0152acf710ab143414d15f5e2fac27d7
devonhackley <devonhackley@gmail.com>
2017-09-11 12:46:38 -0700

    working thru some react

---
+++ react-prac/react-loop.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Hello = React.createClass({
+  render: function() {
+    var names = ['Joey', 'Josh', 'Jimmy'];
+    return (
+      <ul>
+        {names.map((name,index) => {
+          return <li key={ index }>{name}</li>;
+        })}
+      </ul>
+    )
+  }
+});
+
+//Best practice
+
+var Hello = React.createClass({
+  render: function(){
+    var names = ['Joey', 'Josh', 'Jimmy'];
+    var namesList = names.map(name => {
+      return <li>{name}</li>
+    })
+
+    return <ul>{ nameList }</ul>
+  }
+})
